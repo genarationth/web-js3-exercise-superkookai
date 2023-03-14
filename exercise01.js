87eb8113118a0ac2596c32ab883b7c3c31fa859e
superkookai <weerawut@live.com>
2023-03-14 13:42:25 +0700

    done exercise01

---
+++ exercise01.js
@@ -0,0 +1,8 @@
+function calculateCost(cost){
+    if (cost <= 0){
+        return 'Enter cost in Positive number';
+    }
+    return cost + 3 + (cost * 0.01); 
+}
+
+console.log(calculateCost(-5));
