5938cd87b0280c42f67f063a4359eccb677bcaf8
superkookai <weerawut@live.com>
2023-03-14 14:08:56 +0700

    corrected exercise01

--- exercise01.js
+++ exercise01.js
@@ -2,7 +2,7 @@ function calculateCost(cost){
     if (cost <= 0){
         return 'Enter cost in Positive number';
     }
-    return cost + 3 + (cost * 0.01); 
+    return cost + 3 + (cost * 0.01 / 100); 
 }
 
 console.log(calculateCost(-5));
